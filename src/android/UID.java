77347b31cda8061f96d84f70b93bc85f3637f803
Keust Pablo Silvano <keust@MacBook-Pro-de-Keust.local>
2019-01-13 19:04:47 -0200

    Melhorias para funcionamento com mudança da forma de buscar o MAC no Android 6.

--- src/android/UID.java
+++ src/android/UID.java
@@ -1,15 +1,13 @@
-/*
- * Copyright (c) 2014 HygieiaSoft
- * Distributed under the MIT License.
- * (See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT)
- */
 package org.hygieiasoft.cordova.uid;
 
+import android.Manifest;
 import android.content.Context;
 import android.telephony.TelephonyManager;
 import android.provider.Settings;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaInterface;
@@ -19,13 +17,26 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import android.util.Log;
+
+import java.lang.reflect.Method;
 
 public class UID extends CordovaPlugin {
+
+	public static final String TAG = "UID";
+	public CallbackContext callbackContext;
+	public static final int REQUEST_READ_PHONE_STATE = 0;
+
 	public static String uuid; // Device UUID
 	public static String imei; // Device IMEI
 	public static String imsi; // Device IMSI
 	public static String iccid; // Sim IMSI
 	public static String mac; // MAC address
+
+	protected final static String permission = Manifest.permission.READ_PHONE_STATE;
 
 	/**
 	 * Constructor.
@@ -43,12 +54,12 @@ public class UID extends CordovaPlugin {
 	@Override
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 		super.initialize(cordova, webView);
-		Context context = cordova.getActivity().getApplicationContext();
-		UID.uuid = getUuid(context);
-		UID.imei = getImei(context);
-		UID.imsi = getImsi(context);
-		UID.iccid = getIccid(context);
-		UID.mac = getMac(context);
+		// Context context = cordova.getActivity().getApplicationContext();
+		// UID.uuid = getUuid(context);
+		// UID.imei = getImei(context);
+		// UID.imsi = getImsi(context);
+		// UID.iccid = getIccid(context);
+		// UID.mac = getMac(context);
 	}
 
 	/**
@@ -61,17 +72,23 @@ public class UID extends CordovaPlugin {
 	 */
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		if (action.equals("getUID")) {
-			JSONObject r = new JSONObject();
-			r.put("UUID", UID.uuid);
-			r.put("IMEI", UID.imei);
-			r.put("IMSI", UID.imsi);
-			r.put("ICCID", UID.iccid);
-			r.put("MAC", UID.mac);
-			callbackContext.success(r);
-		} else {
-			return false;
-		}
+		this.callbackContext = callbackContext;
+		try {
+			if (action.equals("getUID")) {
+				// permission work
+				if(this.hasPermission(permission)) {
+					dispatchValues();
+				} else {
+					this.requestPermission(this, REQUEST_READ_PHONE_STATE, permission);
+				}
+			} else {
+				this.callbackContext.error("Invalid action");
+				return false;
+			}
+		} catch(Exception e ) {
+            this.callbackContext.error("Exception occurred: ".concat(e.getMessage()));
+            return false;
+        }
 		return true;
 	}
 
@@ -129,10 +146,89 @@ public class UID extends CordovaPlugin {
 	 * @return
 	 */
 	public String getMac(Context context) {
-		final WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-		final WifiInfo wInfo = wifiManager.getConnectionInfo();
-		String mac = wInfo.getMacAddress();
+		String mac = "";
+		if (android.os.Build.VERSION.SDK_INT >= 23) {
+     		// only for marshmallow and newer versions
+			try {
+				List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+				for (NetworkInterface nif : all) {
+					if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+	
+					byte[] macBytes = nif.getHardwareAddress();
+					if (macBytes == null) {
+						return "";
+					}
+	
+					StringBuilder res1 = new StringBuilder();
+					for (byte b : macBytes) {
+						res1.append(Integer.toHexString(b & 0xFF) + ":");
+					}
+	
+					if (res1.length() > 0) {
+						res1.deleteCharAt(res1.length() - 1);
+					}
+					mac = res1.toString();
+				}
+			} catch (Exception ex) {
+        	}
+		} else {
+			final WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+			final WifiInfo wInfo = wifiManager.getConnectionInfo();
+			mac = wInfo.getMacAddress();
+		}
+		
 		return mac;
 	}
+
+	public void onRequestPermissionResult(int requestCode, String[] permissions,
+                                          int[] grantResults) throws JSONException {
+        if(requestCode == REQUEST_READ_PHONE_STATE){
+            dispatchValues();
+        }
+    }
+
+	protected void dispatchValues() {
+		try {
+			Context context = cordova.getActivity().getApplicationContext();
+			UID.uuid = getUuid(context);
+			UID.imei = getImei(context);
+			UID.imsi = getImsi(context);
+			UID.iccid = getIccid(context);
+			UID.mac = getMac(context);
+
+		
+			JSONObject r = new JSONObject();
+			r.put("UUID", UID.uuid);
+			r.put("IMEI", UID.imei);
+			r.put("IMSI", UID.imsi);
+			r.put("ICCID", UID.iccid);
+			r.put("MAC", UID.mac);
+			this.callbackContext.success(r);
+		} catch(Exception e) {
+			this.callbackContext.error("Exception occurred: ".concat(e.getMessage()));
+		}
+	}
+
+	private boolean hasPermission(String permission) throws Exception{
+        boolean hasPermission = true;
+        Method method = null;
+        try {
+            method = cordova.getClass().getMethod("hasPermission", permission.getClass());
+            Boolean bool = (Boolean) method.invoke(cordova, permission);
+            hasPermission = bool.booleanValue();
+        } catch (NoSuchMethodException e) {
+            Log.w(TAG, "Cordova v" + CordovaWebView.CORDOVA_VERSION + " does not support API 23 runtime permissions so defaulting to GRANTED for " + permission);
+        }
+        return hasPermission;
+    }
+
+    private void requestPermission(CordovaPlugin plugin, int requestCode, String permission) throws Exception{
+        try {
+            java.lang.reflect.Method method = cordova.getClass().getMethod("requestPermission", org.apache.cordova.CordovaPlugin.class ,int.class, java.lang.String.class);
+            method.invoke(cordova, plugin, requestCode, permission);
+        } catch (NoSuchMethodException e) {
+            throw new Exception("requestPermission() method not found in CordovaInterface implementation of Cordova v" + CordovaWebView.CORDOVA_VERSION);
+        }
+    }
 
 }
